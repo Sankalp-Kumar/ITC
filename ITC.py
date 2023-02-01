7df603d1c1548f46bf5bb08ff2e71a86b2561204
Sankalp Kumar <41043701+Sankalp-Kumar@users.noreply.github.com>
2023-02-01 14:18:02 +0530

    Add files via upload

---
+++ ITC.py
@@ -0,0 +1,15 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense, Dropout
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import seaborn as sns
+from warnings import filterwarnings
+filterwarnings('ignore')
+import tensorflow as tf
+import streamlit as st
+
+
+st.title('DEMAND FORECASTING ON ITC DATA')
